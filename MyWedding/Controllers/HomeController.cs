6685089e7f2485796c808f78559bf8860eecef86
Ivan De wit <deWit.Ivan777@gmail.com>
2019-05-01 11:10:00 +0200

    Initial

---
+++ MyWedding/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyWedding.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyWedding.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+     
+        public IActionResult RSVP(RSVP model)
+        {
+            return View("~/Views/Home/Index.cshtml",model);
+
+        }
+    }
+}
---
+++ MyWedding/wwwroot/lib/Site.js
@@ -0,0 +1,27 @@
+﻿//Iniciate WOW.js
+new WOW().init();
+
+//Smooth Scrolling
+$('nav a').bind('click', function () {
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 2000, 'easeInOutExpo');
+    event.preventDefault();
+})
+
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function () {
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function () {
+    $("#myCarousel").carousel("prev");
+});
+
+
+$('.collapse').collapse();
+
